126d904eb5117c3de95a99698ad84b66e19420c8
Max2409 <maksimmezuev242@gmail.com>
2026-05-10 15:23:12 +0300

    Межуев Максим 25-ИСТ-4-1

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,57 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& d) : data(d), next(nullptr) {}
+    };
+
+    Node* head;  
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (!empty()) {
+            pop();
+        }
+    }
+
+    // Вставка элемента в очередь за O(n)
+    void push(const T& val) {
+        Node* newNode = new Node(val);
+
+        if (!head || val.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+
+        Node* cur = head;
+        while (cur->next && cur->next->data.prior >= val.prior) {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+    }
+
+    T pop() {
+        if (!head) {
+            throw std::runtime_error("TPQueue: попытка извлечения из пустой очереди");
+        }
+        Node* temp = head;
+        T result = head->data;
+        head = head->next;
+        delete temp;
+        return result;
+    }
+
+    // Проверка очереди на пустоту
+    bool empty() const {
+        return head == nullptr;
+    }
 };
 
 struct SYM {
